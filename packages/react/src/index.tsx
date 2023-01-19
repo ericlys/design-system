747e65613f0eb36cef3add3d6c55cc0e7d716e2b
Ericlys Moreira <ericlysm@gmail.com>
2023-01-18 22:30:51 -0300

    chore: add story button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,35 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  background: '$ignite500',
-  borderRadius: '$md',
-  padding: '$4',
-  height: '$10',
+  background: '$ignite300',
+  borderRadius: '$sm',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+// export type ButtonProps = {
+//   size: 'small' | 'big'
+// }
+
+export type ButtonProps = ComponentProps<typeof Button>
