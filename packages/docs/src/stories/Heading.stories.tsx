47b854e6299b34406ac5640ac011bed67f9f1514
Ericlys Moreira <ericlysm@gmail.com>
2023-01-19 19:01:37 -0300

    chore: Heading component

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ipsa excepturi officia fuga quam distinctio illum suscipit, et sint minus, iste totam. Adipisci, perferendis. Neque consectetur molestiae nisi ducimus praesentium.',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão o heading sempre será um `h2`, mas podemos alterar isso com a propriedade `as`',
+      },
+    },
+  },
+}
