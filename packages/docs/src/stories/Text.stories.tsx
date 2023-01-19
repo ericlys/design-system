d0ba96cc4e5405fd6aac038639a982049b2fdcbe
Ericlys Moreira <ericlysm@gmail.com>
2023-01-19 18:45:45 -0300

    chore: Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae ipsa excepturi officia fuga quam distinctio illum suscipit, et sint minus, iste totam. Adipisci, perferendis. Neque consectetur molestiae nisi ducimus praesentium.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
